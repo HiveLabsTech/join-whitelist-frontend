d419c273cea77ff08cf401cac7f9bf9c3ba68990
pengfan <450652673@qq.com>
2024-08-09 10:04:52 +0800

    fix: 修改域名ip

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const doMain = process.env.NODE_ENV === "development" ? process.env.NEXT_PUBLIC_DEV_PROXY_BASEURL : process.env.NEXT_PUBLIC_PRO_PROXY_BASEURL
+const doMain = process.env.NODE_ENV === "development" ? process.env.NEXT_PUBLIC_DEV_PROXY_BASEURL : process.env.NEXT_PUBLIC_CROSS_DOMAIN_PROXY_IP
 
 
 const nextConfig = {
