c4f48ec71f1cdd388d6abe5e35525ab382b23388
pengfan <450652673@qq.com>
2024-08-12 10:59:26 +0800

    调试

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -43,14 +43,17 @@ export async function POST(req: NextRequest): Promise<Response> {
     if(result.data) {
         // 需要满足的条件：关注 + 加入频道
         const condtion1 = (Number(result.data.follow_fid) > -1) && (result.data.channel_id)
+        // 只需要满足follow条件即可加入
+        const condition2 = (Number(result.data.follow_fid) > -1) && !(result.data.channel_id)
+        // 只需要满足joined channel即可加入
+        const condition3 = !(Number(result.data.follow_fid) > -1) && (result.data.channel_id)
 
         // 用户本身是否满足的条件
         const isFollowed = result.data.isFollowCondition // true：已关注 false 未关注
         const isJoinedChannel = result.data.isChannelCondition // false：是否已加入频道
       
-
+        // 满足 关注 + 加入频道 && 未关注 && 未加入
         if(condtion1 && !isFollowed && !isJoinedChannel) {
-            // 满足 关注 + 加入频道 && 未关注 && 未加入
             return new NextResponse(
                 getFrameHtmlResponse({
                     buttons: [
@@ -65,96 +68,48 @@ export async function POST(req: NextRequest): Promise<Response> {
                     post_url: `${NEXT_PUBLIC_URL}/api/frame?pageType=2&indexType=channel&projectId=${projectId}`
                 })
             )
+        } else if(condtion1 && !isFollowed && isJoinedChannel || condition2 && !isFollowed) { // 需满足两个条件且未关注且已加入频道 或者 只需满足关注条件且为关注的状态
+            return new NextResponse(
+                getFrameHtmlResponse({
+                    buttons: [
+                        {
+                            label: `follow_${fid}`, 
+                        }
+                    ],
+                    image: `${imgUrl}`,
+                    post_url: `${NEXT_PUBLIC_URL}/api/frame?pageType=2&indexType=follow&projectId=${projectId}`
+                })
+            )
+        } else if(condtion1 && isFollowed && !isJoinedChannel || condition3 && !isJoinedChannel) {
+            // 需满足两个条件且已关注且未加入频道 或者 只需满足加入频道且为未加入频道
+            return new NextResponse(
+                getFrameHtmlResponse({
+                    buttons: [
+                        {
+                            label: `follow_${fid}`, 
+                        }
+                    ],
+                    image: `${imgUrl}`,
+                    post_url: `${NEXT_PUBLIC_URL}/api/frame?pageType=2&indexType=channel&projectId=${projectId}`
+                })
+            )
+        } 
+    } 
 
-        }
-    }
-
+    // 加入成功
     return new NextResponse(
         getFrameHtmlResponse({
             buttons: [
                 {
-                    label: `join_channel_${fid}`,
+                    label: `read more`,
                 }
             ],
             image: `${imgUrl}`,
-            post_url: `${NEXT_PUBLIC_URL}/api/frame?pageType=2&indexType=channel&projectId=${projectId}`
+            post_url: `${NEXT_PUBLIC_URL}/api/frame?pageType=2&indexType=readmore&projectId=${projectId}`
         })
     )
 
-    // if (buttonId == 1) {
-
-    //     if (result.data) {
-    //         // 有两个满足条件
-    //         const condtion1 = Number(result.data.follow_id) > -1 && result.data.channel_id
-    //         // 只有一个条件follow
-    //         const condition2 = Number(result.data.follow_id) > -1 && !result.data.channel_id
-    //         // 只有一个条件joined channel
-    //         const condition3 = !(Number(result.data.follow_id) > -1) && result.data.channel_id
-
-
-    //         // 未关注
-    //         const followNotFinished = !result.data.isFollowCondition
-    //         // 未joined channel
-    //         const channelNotFinished = !result.data.isChannelCondition
-
-    //         // 需要满足follow joined_channel && 都是 unfollow unChannel
-    //         if (condtion1 && followNotFinished && channelNotFinished) {
-    //             return new NextResponse(
-    //                 getFrameHtmlResponse({
-    //                     buttons: [
-    //                         {
-    //                             label: `follow`,
-    //                         },
-    //                         {
-    //                             label: `join_channel`,
-    //                         }
-    //                     ],
-    //                     image: `${imgUrl}`,
-    //                     post_url: `${NEXT_PUBLIC_URL}/api/frame?pageType=2&indexType=follow&projectId=${projectId}`
-    //                 })
-    //             )
-    //         } else if (condtion1 && followNotFinished && !channelNotFinished || condition2 && followNotFinished) {
-    //             return new NextResponse(
-    //                 getFrameHtmlResponse({
-    //                     buttons: [
-    //                         {
-    //                             label: `follow`,
-    //                         },
-    //                     ],
-    //                     image: `${imgUrl}`,
-    //                     post_url: `${NEXT_PUBLIC_URL}/api/frame?pageType=2&indexType=follow&projectId=${projectId}`
-    //                 })
-    //             )
-    //         } else if (condtion1 && !followNotFinished && channelNotFinished || condition3 && channelNotFinished) {
-    //             return new NextResponse(
-    //                 getFrameHtmlResponse({
-    //                     buttons: [
-    //                         {
-    //                             label: `join_channel_${fid}`,
-    //                         }
-    //                     ],
-    //                     image: `${imgUrl}`,
-    //                     post_url: `${NEXT_PUBLIC_URL}/api/frame?pageType=2&indexType=channel&projectId=${projectId}`
-    //                 })
-    //             )
-    //         } else {
-    //             // 不需要任何条件即可加入
-    //             return new NextResponse(
-    //                 getFrameHtmlResponse({
-    //                     buttons: [
-    //                         {
-    //                             label: `read more`,
-    //                         }
-    //                     ],
-    //                     image: `${imgUrl}`,
-    //                     post_url: `${NEXT_PUBLIC_URL}/api/frame?pageType=2&indexType=readmore&projectId=${projectId}`
-    //                 })
-    //             )
-    //         }
-    //     }
-
-
-    // }
+    
 
     const headers = new Headers()
     headers.set('Loaction', NEXT_PUBLIC_URL)
