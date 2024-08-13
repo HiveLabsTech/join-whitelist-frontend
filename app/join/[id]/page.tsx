9e2ac0567220cb6f9de0c01e252ead9c92ebc408
pengfan <450652673@qq.com>
2024-08-13 17:24:22 +0800

    fix: 调试

--- app/join/[id]/page.tsx
+++ app/join/[id]/page.tsx
@@ -3,14 +3,14 @@ import { NEXT_PUBLIC_URL } from '@/app/config';
 import ProjectService from '@/app/service/projectService';
 
 
-export default async function Page({ params}: {params: { id: number | string }}) {
+export default async function Page({ params }: { params: { id: number | string } }) {
     let imgUrl: string = ""
     const joinId = params.id
     try {
         const result = await ProjectService.getProjectInfoImage(joinId)
         imgUrl = result.message
         console.log(imgUrl)
-    }catch(error) {
+    } catch (error) {
         console.log(error)
     }
 
@@ -20,18 +20,18 @@ export default async function Page({ params}: {params: { id: number | string }})
                 buttons={
                     [
                         {
-                          label: `Join`,
-                          action: 'post'
+                            label: `Join`,
+                            action: 'post'
                         },
-                      ]
+                    ]
                 }
                 image={imgUrl}
                 post_url={`${NEXT_PUBLIC_URL}/api/frame?pageType=1&projectId=${joinId}`}
             ></FrameMetadata>
             <div className='w-[100vw] h-[100vh] flex'>
-                
+
                 <img src={imgUrl} alt="" className='w-[1200px] h-[630px] m-auto' />
-        </div>
+            </div>
         </>
-    )   
+    )
 }
