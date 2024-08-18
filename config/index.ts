552563533bc92202babbf401288cf191a8e7f536
pengfan <450652673@qq.com>
2024-08-18 11:50:40 +0800

    新增未登录时点击join按钮跳转登录功能

--- config/index.ts
+++ config/index.ts
@@ -1,11 +1,14 @@
+const RPC_URL = process.env.NEXT_PUBLIC_RPC_URL
+const APIKEY = process.env.NEXT_PUBLIC_API_KEY
+
 export const SIGNIN_BUTTON_CONFIG = {
-    rpcUrl: "https://rpc.ankr.com/optimism/e89062333443f0a74b95c0bed25e3d8b6c50a25f93c146061579084df934095c",
+    rpcUrl: RPC_URL,
     domain: "joinwhitelist.xyz",
     siweUri: "https://joinwhitelist.xyz/login",
     relay: 'https://relay.farcaster.xyz',
 }
 
-export const API_KEY = "F498BF9A-42CC-4B91-88F2-45AC2624EEB5"
+export const API_KEY = APIKEY
 
 
 
