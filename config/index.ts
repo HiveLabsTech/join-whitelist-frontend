e6d9957e0c081cca13e4e73fa8f6eeced2b6f04e
pengfan <450652673@qq.com>
2024-08-21 10:09:53 +0800

    新增图片上传token

--- config/index.ts
+++ config/index.ts
@@ -8,7 +8,7 @@ export const SIGNIN_BUTTON_CONFIG = {
     relay: 'https://relay.farcaster.xyz',
 }
 
-export const API_KEY = "F498BF9A-42CC-4B91-88F2-45AC2624EEB5"
+export const API_KEY = APIKEY
 
 
 
